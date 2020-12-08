362d58f1f65c0ae304e7a53932950e9d461e4533
AlexVipond <alex.r.vipond@gmail.com>
2020-12-08 00:07:09 -0600

    removes loggers

--- src/affordances/useListeners.js
+++ src/affordances/useListeners.js
@@ -2,17 +2,14 @@ import { onMounted, onBeforeUnmount } from 'vue'
 import { catchWithNextTick } from '../util'
 
 export default function useListeners ({ target, listeners }) {
-  console.log({ target, listeners })
   Object.entries(listeners).forEach(([eventType, rawListener]) => {
     const { callback, options } = ensureListener(rawListener)
 
     onMounted(() => {
-      console.log('onMounted')
       catchWithNextTick(() => target.value.addEventListener(eventType, callback, options))
     })
 
     onBeforeUnmount(() => {
-      console.log('onBeforeUnmount')
       catchWithNextTick(() => target.value.removeEventListener(eventType, callback, options))
     })
   })
