5c292bdb3b3568c63426d896c8248f65c5e76792
AlexVipond <alex.r.vipond@gmail.com>
2021-02-02 21:45:29 -0600

    organizes by logical concern

--- src/functions/useTablist.js
+++ src/functions/useTablist.js
@@ -14,11 +14,14 @@ const defaultOptions = {
 //   selectsPanelOnTabFocus?: boolean,
 //   openMenu?: () => void,
 //   deleteTab?: () => void,
-//   ariaLabel: string,
+//   label?: string,
+//   openMenuKeycombo?: Listenable.Keycombo,
+//   deleteTabKeycombo?: Listenable.Keycombo,
+//   transition?: { panel?: Transition },
 // }
 
 export default function useTablist ({ totalTabs, orientation }, options = {}) {
-  // Process arguments
+  // PARAMETER PROCESSING
   const eachable = toEachable(totalTabs),
         {
           selectsPanelOnTabFocus,
@@ -30,120 +33,29 @@ export default function useTablist ({ totalTabs, orientation }, options = {}) {
           transition,
         } = { ...defaultOptions, ...options }
 
-  // Set up core state
+
+  // TARGET SETUP
+  const root = useTarget('single'),
+        label = useTarget('single'),
+        tabs = useTarget('multiple'),
+        panels = useTarget('multiple')
+
+  
+  // SELECTED TAB
   const navigateable = useNavigateable(eachable.value),
-        selectedPanel = ref(navigateable.value.location),
         selectedTab = computed({
           get: () => navigateable.value.location,
           set: location => navigateable.value.navigate(location)
-        })
-  
-  if (selectsPanelOnTabFocus) {
-    watch(
-      () => selectedTab.value,
-      () => selectedPanel.value = selectedTab.value,
-    )
-  }
+        }),
+        tabFocusUpdates = ref(0),
+        forceTabFocusUpdate = () => tabFocusUpdates.value++
 
   watch(
     () => eachable.value,
     () => navigateable.value.setArray(eachable.value),
     { flush: 'post' }
   )
-  
-  // Set up API
-  const root = useTarget('single'),
-        label = useTarget('single'),
-        tabs = (() => {
-          const { targets, handle } = useTarget('multiple'),
-                htmlIds = useId({ target: targets, watchSources: eachable })
-
-          return { targets, handle, htmlIds }
-        })(),
-        panels = (() => {
-          const { targets, handle } = useTarget('multiple'),
-                htmlIds = useId({ target: targets, watchSources: eachable })
-
-          return { targets, handle, htmlIds }
-        })(),
-        labelId = ariaLabel ? undefined : useId({ target: label.target })
-
-
-  // Bind accessibility attributes
-  if (!ariaLabel) {
-    // If there is no ariaLabel, a ariaLabel target is required for accessibility.
-    // This code will throw an error otherwise.
-    useBindings({
-      target: label.target,
-      bindings: { id: labelId },
-    })
-  }
-  
-  useBindings({
-    target: root.target,
-    bindings: {
-      // The element that serves as the container for the set of tabs has role tablist. 
-      role: 'tablist',
-      // If the tablist element is vertically oriented, it has the property aria-orientation set to vertical. The default value of aria-orientation for a tablist element is horizontal. 
-      ariaOrientation: orientation,
-      // If the tab list has a visible label, the element with role tablist has aria-labelledby set to a value that refers to the labelling element. Otherwise, the tablist element has a label provided by aria-label. 
-      [ariaLabel ? 'ariaLabel' : 'ariaLabelledby']: ariaLabel || labelId,
-    }
-  })
-
-  // tabs
-  useBindings({
-    target: tabs.targets,
-    bindings: {
-      tabindex: 0,
-      id: ({ index }) => tabs.htmlIds.value[index],
-      // Each element that serves as a tab has role tab and is contained within the element with role tablist.
-      role: 'tab',
-      // Each element with role tab has the property aria-controls referring to its associated tabpanel element.
-      ariaControls: ({ index }) => panels.htmlIds.value[index],
-      // The active tab element has the state aria-selected set to true and all other tab elements have it set to false.
-      ariaSelected: {
-        targetClosure: ({ index }) => index === selectedTab.value,
-        watchSources: selectedTab,
-      },
-      // If a tab element has a pop-up menu, it has the property aria-haspopup set to either menu or true. 
-      ariaHaspopup: !!openMenu,
-    },
-  })
-
-  // panels
-  useBindings({
-    target: panels.targets,
-    bindings: {
-      id: ({ index }) => panels.htmlIds.value[index],
-      // Each element that contains the content panel for a tab has role tabpanel.
-      role: 'tabpanel',
-      // Each element with role tabpanel has the property aria-labelledby referring to its associated tab element. 
-      ariaLabelledby: ({ index }) => tabs.htmlIds.value[index],
-      ariaHidden: {
-        targetClosure: ({ index }) => index !== selectedPanel.value,
-        watchSources: selectedPanel,
-      },
-    },
-  })
-
-
-  // Manage panel visibility
-  useConditionalDisplay(
-    {
-      target: panels.targets,
-      condition: {
-        targetClosure: ({ index }) => index === selectedPanel.value,
-        watchSources: selectedPanel,
-      }
-    },
-    { transition: transition?.panel }
-  )
-
-  
-  // Manage navigateable tab
-  const tabFocusUpdates = ref(0),
-        forceTabFocusUpdate = () => tabFocusUpdates.value++
+        
   onMounted(() => {
     watch(
       [
@@ -165,15 +77,6 @@ export default function useTablist ({ totalTabs, orientation }, options = {}) {
   useListenables({
     target: tabs.targets,
     listenables: {
-      mousedown: {
-        targetClosure: ({ index }) => () => {
-          navigateable.value.navigate(index)
-          if (!selectsPanelOnTabFocus) {
-            selectedPanel.value = selectedTab.value
-          }
-        }
-      },
-
       // When focus moves into the tab list, places focus on the tab that controls the selected tab panel.
       focusin: {
         targetClosure: ({ index }) => event => {
@@ -231,7 +134,54 @@ export default function useTablist ({ totalTabs, orientation }, options = {}) {
             }
         }
       })(),
-      
+
+      // Home (Optional): Moves focus to the first tab.
+      home (event) {
+        event.preventDefault()
+        navigateable.value.first()
+      },
+      // End (Optional): Moves focus to the last tab.
+      end (event) {
+        event.preventDefault()
+        navigateable.value.last()
+      },
+    },
+  })
+
+
+  // SELECTED PANEL
+  const selectedPanel = ref(navigateable.value.location)
+
+  if (selectsPanelOnTabFocus) {
+    watch(
+      () => selectedTab.value,
+      () => selectedPanel.value = selectedTab.value,
+    )
+  }
+
+  useConditionalDisplay(
+    {
+      target: panels.targets,
+      condition: {
+        targetClosure: ({ index }) => index === selectedPanel.value,
+        watchSources: selectedPanel,
+      }
+    },
+    { transition: transition?.panel }
+  )
+
+  useListenables({
+    target: tabs.targets,
+    listenables: {
+      mousedown: {
+        targetClosure: ({ index }) => () => {
+          navigateable.value.navigate(index)
+          if (!selectsPanelOnTabFocus) {
+            selectedPanel.value = selectedTab.value
+          }
+        }
+      },
+
       // Space or Enter: Activates the tab if it was not selected automatically on focus.
       ...(() => 
         selectsPanelOnTabFocus
@@ -247,60 +197,116 @@ export default function useTablist ({ totalTabs, orientation }, options = {}) {
               }
             }
       )(),
+    },
+  })
 
-      // Home (Optional): Moves focus to the first tab.
-      home (event) {
-        event.preventDefault()
-        navigateable.value.first()
-      },
-      // End (Optional): Moves focus to the last tab.
-      end (event) {
-        event.preventDefault()
-        navigateable.value.last()
-      },
-      
-      // Shift + F10: If the tab has an associated pop-up menu, opens the menu.
-      ...(() => {
-        return openMenu
-          ? {
-              [openMenuKeycombo]: function (event) {
-                event.preventDefault()
-                openMenu(navigateable.value.location)
-              }
+
+  // MULTIPLE CONCERNS
+  if (deleteTab) {
+    useListenables({
+      target: tabs.targets,
+      listenables: {
+        // Delete (Optional): If deletion is allowed, deletes (closes) the current tab element and its associated tab panel, sets focus on the tab following the tab that was closed, and optionally activates the newly focused tab. If there is not a tab that followed the tab that was deleted, e.g., the deleted tab was the right-most tab in a left-to-right horizontal tab list, sets focus on and optionally activates the tab that preceded the deleted tab. If the application allows all tabs to be deleted, and the user deletes the last remaining tab in the tab list, the application moves focus to another element that provides a logical work flow. As an alternative to Delete, or in addition to supporting Delete, the delete function is available in a context menu. 
+        [deleteTabKeycombo]: function (event) {
+          event.preventDefault()
+          
+          const cached = navigateable.value.location
+          deleteTab(navigateable.value.location)
+          
+          nextTick(() => {
+            navigateable.value.navigate(cached)
+            forceTabFocusUpdate()
+  
+            if (!selectsPanelOnTabFocus && selectedPanel.value === cached) {
+              selectedPanel.value = navigateable.value.location
             }
-          : {}
-      })(),
-      
-      // Delete (Optional): If deletion is allowed, deletes (closes) the current tab element and its associated tab panel, sets focus on the tab following the tab that was closed, and optionally activates the newly focused tab. If there is not a tab that followed the tab that was deleted, e.g., the deleted tab was the right-most tab in a left-to-right horizontal tab list, sets focus on and optionally activates the tab that preceded the deleted tab. If the application allows all tabs to be deleted, and the user deletes the last remaining tab in the tab list, the application moves focus to another element that provides a logical work flow. As an alternative to Delete, or in addition to supporting Delete, the delete function is available in a context menu. 
-      ...(() => {
-        return deleteTab
-          ? {
-              [deleteTabKeycombo]: function (event) {
-                event.preventDefault()
-                
-                const cached = navigateable.value.location
-                deleteTab(navigateable.value.location)
-                
-                nextTick(() => {
-                  navigateable.value.navigate(cached)
-                  forceTabFocusUpdate()
-
-                  if (!selectsPanelOnTabFocus && selectedPanel.value === cached) {
-                    selectedPanel.value = navigateable.value.location
-                  }
-
-                  if (!selectsPanelOnTabFocus && selectedPanel.value > navigateable.value.array.length - 1) {
-                    selectedPanel.value = selectedPanel.value - 1
-                  }
-
-                })
-              }
+  
+            if (!selectsPanelOnTabFocus && selectedPanel.value > navigateable.value.array.length - 1) {
+              selectedPanel.value = selectedPanel.value - 1
             }
-          : {}
-      })(),
+  
+          })
+        },
+      },
+    })
+  }
+
+
+  // WAI ARIA BASICS
+  const labelId = ariaLabel ? undefined : useId({ target: label.target }),
+        tabIds = useId({ target: tabs.targets, watchSources: eachable }),
+        panelIds = useId({ target: panels.targets, watchSources: eachable })
+
+  if (!ariaLabel) {
+    // If there is no ariaLabel, a ariaLabel target is required for accessibility.
+    // This code will throw an error otherwise.
+    useBindings({
+      target: label.target,
+      bindings: { id: labelId },
+    })
+  }
+  
+  useBindings({
+    target: root.target,
+    bindings: {
+      // The element that serves as the container for the set of tabs has role tablist. 
+      role: 'tablist',
+      // If the tablist element is vertically oriented, it has the property aria-orientation set to vertical. The default value of aria-orientation for a tablist element is horizontal. 
+      ariaOrientation: orientation,
+      // If the tab list has a visible label, the element with role tablist has aria-labelledby set to a value that refers to the labelling element. Otherwise, the tablist element has a label provided by aria-label. 
+      [ariaLabel ? 'ariaLabel' : 'ariaLabelledby']: ariaLabel || labelId,
     }
   })
 
+  useBindings({
+    target: tabs.targets,
+    bindings: {
+      tabindex: 0,
+      id: ({ index }) => tabIds.value[index],
+      // Each element that serves as a tab has role tab and is contained within the element with role tablist.
+      role: 'tab',
+      // Each element with role tab has the property aria-controls referring to its associated tabpanel element.
+      ariaControls: ({ index }) => panelIds.value[index],
+      // The active tab element has the state aria-selected set to true and all other tab elements have it set to false.
+      ariaSelected: {
+        targetClosure: ({ index }) => index === selectedTab.value,
+        watchSources: selectedTab,
+      },
+      // If a tab element has a pop-up menu, it has the property aria-haspopup set to either menu or true. 
+      ariaHaspopup: !!openMenu,
+    },
+  })
+
+  useBindings({
+    target: panels.targets,
+    bindings: {
+      id: ({ index }) => panelIds.value[index],
+      // Each element that contains the content panel for a tab has role tabpanel.
+      role: 'tabpanel',
+      // Each element with role tabpanel has the property aria-labelledby referring to its associated tab element. 
+      ariaLabelledby: ({ index }) => tabIds.value[index],
+      ariaHidden: {
+        targetClosure: ({ index }) => index !== selectedPanel.value,
+        watchSources: selectedPanel,
+      },
+    },
+  })
+
+  if (openMenu) {
+    useListenables({
+      target: tabs.targets,
+      listenables: {
+        // Shift + F10: If the tab has an associated pop-up menu, opens the menu.
+        [openMenuKeycombo]: function (event) {
+          event.preventDefault()
+          openMenu(navigateable.value.location)
+        },
+      },
+    })
+  }
+
+
+  // API
   const tablist = {
     root: root.handle,
     tabs: tabs.handle,
