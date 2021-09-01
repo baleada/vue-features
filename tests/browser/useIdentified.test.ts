ab578074acf8aad4000ed8c0de92b8e930346835
AlexVipond <alex.r.vipond@gmail.com>
2021-09-01 10:33:20 -0500

    test useIdentified

---
+++ tests/browser/useIdentified.test.ts
@@ -0,0 +1,52 @@
+import { suite as createSuite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { withPuppeteer } from '@baleada/prepare'
+import { WithGlobals } from '../fixtures/types'
+
+const suite = withPuppeteer(
+  createSuite('useIdentified')
+)
+
+suite(`assigns unique ID to identified element`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useIdentified/calledRef')
+  await page.waitForSelector('span')
+  
+  const value = await page.evaluate(async () => {
+          return (window as unknown as WithGlobals).testState.identified.element.value.id.length
+        }),
+        expected = 21
+
+  assert.is(value, expected)
+})
+
+suite(`assigns matching unique ID to the property that identifies the element`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useIdentified/calledRef')
+  await page.waitForSelector('span')
+  
+  const value = await page.evaluate(async () => {
+          return {
+            id: (window as unknown as WithGlobals).testState.identified.element.value.id,
+            matching: (window as unknown as WithGlobals).testState.identifying.value.getAttribute(
+              (window as unknown as WithGlobals).testState.attribute
+            )
+          }
+        })
+
+  assert.is(value.id, value.matching)
+})
+
+suite(`prevents effect when identified ref is not called`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useIdentified/notCalledRef')
+  await page.waitForSelector('span')
+  
+  const value = await page.evaluate(async () => {
+          return (window as unknown as WithGlobals).testState.identifying.value.hasAttribute(
+            (window as unknown as WithGlobals).testState.attribute
+          )
+        }),
+        expected = false
+
+  assert.is(value, expected)
+})
+
+suite.run()
---
+++ tests/stubs/app/src/pages/useIdentified/notCalledRef.vue
@@ -0,0 +1,19 @@
+<template>
+  <span ref="stub"></span>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useIdentified } from '../../../../../../src/extracted/useIdentified'
+import { WithGlobals } from '../../../../../fixtures/types';
+
+const stub = ref(null),
+      attribute = 'aria-details',
+      identified = useIdentified({ identifying: stub, attribute });
+
+(window as unknown as WithGlobals).testState = {
+  identified,
+  identifying: stub,
+  attribute,
+}
+</script>
