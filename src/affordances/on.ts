28a69ab26590cf8d0c62285a815e7998130291c5
AlexVipond <alex.r.vipond@gmail.com>
2021-09-14 18:59:53 -0500

    working on more permissive default typing

--- src/affordances/on.ts
+++ src/affordances/on.ts
@@ -4,15 +4,15 @@ import type { Listenable, ListenableOptions, ListenableSupportedType, ListenEffe
 import { ensureElementsRef, schedule, toEntries } from '../extracted'
 import type { Target } from '../extracted'
 
-type DefineOnEffect<Type extends ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = 
+type DefineOnEffect<Type extends ListenableSupportedType = ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = 
   <EffectType extends Type>(type: EffectType, effect: OnEffect<EffectType, RecognizeableMetadata>)
     => [type: Type, effect: OnEffect<Type, RecognizeableMetadata>]
 
 export type OnTarget = Target<HTMLElement | Document | (Window & typeof globalThis)>
 
-export type OnEffect<Type extends ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = ListenEffect<Type> | OnEffectObject<Type, RecognizeableMetadata>
+export type OnEffect<Type extends ListenableSupportedType = ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = ListenEffect<Type> | OnEffectObject<Type, RecognizeableMetadata>
 
-export type OnEffectObject<Type extends ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = {
+export type OnEffectObject<Type extends ListenableSupportedType = ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = {
   createEffect: OnEffectCreator<Type, RecognizeableMetadata>,
   options?: {
     listenable?: ListenableOptions<Type, RecognizeableMetadata>,
@@ -20,7 +20,7 @@ export type OnEffectObject<Type extends ListenableSupportedType, RecognizeableMe
   },
 }
 
-export type OnEffectCreator<Type extends ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = (
+export type OnEffectCreator<Type extends ListenableSupportedType = ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> = (
   { element, index: elementIndex, off }: {
     element: HTMLElement | Document | (Window & typeof globalThis),
     index: number,
@@ -32,7 +32,7 @@ export type OnEffectCreator<Type extends ListenableSupportedType, RecognizeableM
 // TODO: Support modifiers: https://v3.vuejs.org/api/directives.html#v-on
 // Not all are necessary, as Listenable solves a lot of those problems.
 // .once might be worth supporting.
-export function on<Type extends ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> (
+export function on<Type extends ListenableSupportedType = ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> (
   { element, effects }: {
     element: OnTarget,
     effects: Record<Type, OnEffect<Type, RecognizeableMetadata>>
@@ -90,13 +90,13 @@ export function on<Type extends ListenableSupportedType, RecognizeableMetadata e
   // useListenable cleans up side effects automatically
 }
 
-function createDefineOnEffect<Type extends ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> (): DefineOnEffect<Type, RecognizeableMetadata> {
+function createDefineOnEffect<Type extends ListenableSupportedType = ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> (): DefineOnEffect<Type, RecognizeableMetadata> {
   return ((type, effect) => {
     return [type, effect]
   }) as DefineOnEffect<Type, RecognizeableMetadata>
 }
 
-function ensureListenParams<Type extends ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> (rawListenable: OnEffect<Type, RecognizeableMetadata>): OnEffectObject<Type, RecognizeableMetadata> {
+function ensureListenParams<Type extends ListenableSupportedType = ListenableSupportedType, RecognizeableMetadata extends Record<any, any> = Record<any, any>> (rawListenable: OnEffect<Type, RecognizeableMetadata>): OnEffectObject<Type, RecognizeableMetadata> {
   return typeof rawListenable === 'function'
     ? { createEffect: () => rawListenable }
     : {
