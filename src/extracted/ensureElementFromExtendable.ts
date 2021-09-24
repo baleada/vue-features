7619b362bdbe4be022665ff531cdee8f7410aee0
AlexVipond <alex.r.vipond@gmail.com>
2021-09-24 15:17:51 -0500

    simplify extendable type

--- src/extracted/ensureElementFromExtendable.ts
+++ src/extracted/ensureElementFromExtendable.ts
@@ -1,13 +1,19 @@
-import { computed } from 'vue'
+import { computed, isRef } from 'vue'
 import type { Ref } from 'vue'
 import type { SingleElement } from './elementApi'
 
-export type Extendable = { root: SingleElement<HTMLElement> } | (() => HTMLElement)
+export type Extendable = 
+  { root: SingleElement<HTMLElement> }
+  | Ref<HTMLElement>
 
 export function ensureElementFromExtendable (extendable: Extendable): Ref<HTMLElement> {
-  if (typeof extendable !== 'function') {
-    return extendable.root.element
+  if (isRef(extendable)) {
+    return extendable
   }
 
-  return computed(extendable)
+  if (typeof extendable === 'function') {
+    return computed(extendable)
+  }
+
+  return extendable.root.element
 }
