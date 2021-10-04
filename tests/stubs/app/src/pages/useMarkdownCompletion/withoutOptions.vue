7ba6c00739350a2b0ed616224b21b46ab600c68c
AlexVipond <alex.r.vipond@gmail.com>
2021-10-04 00:33:23 -0500

    jamming

--- tests/stubs/app/src/pages/useMarkdownCompletion/withoutOptions.vue
+++ tests/stubs/app/src/pages/useMarkdownCompletion/withoutOptions.vue
@@ -5,12 +5,27 @@
 </template>
 
 <script setup lang="ts">
+import { on } from '../../../../../../src/affordances'
 import { useTextbox } from '../../../../../../src/interfaces'
 import { useMarkdownCompletion } from '../../../../../../src/extensions'
 import { WithGlobals } from '../../../../../fixtures/types'
 
 const textbox = useTextbox(),
-      markdownCompletion = useMarkdownCompletion(textbox);
+      markdownCompletion = useMarkdownCompletion(textbox)
+
+on<any>({
+  element: textbox.root.element,
+  effects: {
+    'cmd+b': event => {
+      event.preventDefault()
+      markdownCompletion.bold()
+    },
+    'shift+cmd+.': event => {
+      event.preventDefault()
+      markdownCompletion.blockquote()
+    },
+  }
+});
 
 (window as unknown as WithGlobals).testState =  { textbox, markdownCompletion }
 </script>
