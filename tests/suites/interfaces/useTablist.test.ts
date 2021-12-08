1552a8da654007e557be8a59490bd0c1adee402a
AlexVipond <alex.r.vipond@gmail.com>
2021-12-07 19:30:47 -0600

    remove useFocusedAndSelected concerns

--- tests/suites/interfaces/useTablist.test.ts
+++ tests/suites/interfaces/useTablist.test.ts
@@ -43,19 +43,6 @@ suite(`aria-orientation is correctly assigned`, async ({ puppeteer: { page } }) 
   assert.is(vertical, 'vertical')
 })
 
-suite(`only selected tab is focusable`, async ({ puppeteer: { page } }) => {
-  await page.goto('http://localhost:3000/useTablist/horizontal')
-  await page.waitForSelector('div')
-
-  const value = await page.evaluate(() => {
-    const divs = [...document.querySelectorAll('div div')],
-          tabs = divs.slice(0, 3)
-    
-    return tabs.map(el => el.getAttribute('tabindex'))
-  })
-  assert.equal(value, ['0', '-1', '-1'])
-})
-
 suite(`tabs' aria-controls match panels' IDs`, async ({ puppeteer: { page } }) => {
   await page.goto('http://localhost:3000/useTablist/horizontal')
   await page.waitForSelector('div')
@@ -96,20 +83,6 @@ suite(`tabs' IDs match panels' aria-labelledby`, async ({ puppeteer: { page } })
   assert.equal(tabs, panels)
 })
 
-suite(`selected tab's aria-selected is true and others are undefined`, async ({ puppeteer: { page } }) => {
-  await page.goto('http://localhost:3000/useTablist/horizontal')
-  await page.waitForSelector('div')
-
-  const tabs = await page.evaluate(async () => {
-          const divs = [...document.querySelectorAll('div div')],
-                tabs = divs.slice(0, 3)
-          
-          return tabs.map(el => el.getAttribute('aria-selected'))
-        })
-
-  assert.equal(tabs, ['true', undefined, undefined])
-})
-
 suite(`selected tab's panel is shown and others are hidden`, async ({ puppeteer: { page } }) => {
   await page.goto('http://localhost:3000/useTablist/horizontal')
   await page.waitForSelector('div')
@@ -136,132 +109,6 @@ suite(`selected tab's panel's aria-hidden is undefined and others are true`, asy
         })
 
   assert.equal(panels, [null, 'true', 'true'])
-})
-
-suite(`selected tab and is.selected functions react to tablist.selected`, async ({ puppeteer: { page } }) => {
-  await page.goto('http://localhost:3000/useTablist/horizontal')
-  await page.waitForSelector('div')
-
-  const value = await page.evaluate(async () => {
-          const divs = [...document.querySelectorAll('div div')],
-                tabs = divs.slice(0, 3),
-                from = {
-                  tabs: tabs.map(el => el.getAttribute('aria-selected')),
-                  is: {
-                    selected: (window as unknown as WithGlobals).testState.tablist.is.selected(1),
-                  },
-                };
-          
-          (window as unknown as WithGlobals).testState.tablist.select.exact(1)
-          await (window as unknown as WithGlobals).nextTick()
-          
-          const to = {
-            tabs: tabs.map(el => el.getAttribute('aria-selected')),
-            is: {
-              selected: (window as unknown as WithGlobals).testState.tablist.is.selected(1),
-            },
-          }
-          
-          return { from, to }
-        })
-
-  assert.equal(value.from.tabs, ['true', undefined, undefined])
-  assert.equal(value.from.is.selected, false)
-  assert.equal(value.to.tabs, [undefined, 'true', undefined])
-  assert.equal(value.to.is.selected, true)
-})
-
-suite(`respects initialSelected option`, async ({ puppeteer: { page } }) => {
-  // Separate route for this one, because initial selection throws off the arithmetic on all the other withOptions tests
-  await page.goto('http://localhost:3000/useTablist/withInitialSelected')
-  await page.waitForSelector('div')
-
-  const value = await page.evaluate(async () => {
-          return (window as unknown as WithGlobals).testState.tablist.selected
-        }),
-        expected = 1
-
-  assert.is(value, expected)
-})
-
-suite(`mousedown on a tab navigates to that tab`, async ({ puppeteer: { page, mouseClick } }) => {
-  await page.goto('http://localhost:3000/useTablist/horizontal')
-  await page.waitForSelector('div')
-
-  const from = await page.evaluate(() => (window as unknown as WithGlobals).testState.tablist.selected)
-  await mouseClick('div div:nth-child(2)')
-  const to = await page.evaluate(() => (window as unknown as WithGlobals).testState.tablist.selected)
-
-  assert.is(from, 0)
-  assert.is(to, 1)
-})
-
-suite(`when selectsOnFocus is false, selected does not reacted to focused`, async ({ puppeteer: { page } }) => {
-  await page.goto('http://localhost:3000/useTablist/withOptions')
-  await page.waitForSelector('div')
-
-  await page.evaluate(async () => document.querySelector('input').focus())
-  await page.keyboard.press('Tab')
-
-  const value = await page.evaluate(async () => {
-          const divs = [...document.querySelectorAll('div div')],
-                tabs = divs.slice(0, 3),
-                from = tabs.map(el => `${document.activeElement.isSameNode(el)}`);
-          
-          (window as unknown as WithGlobals).testState.tablist.focus.exact(1)
-          await (window as unknown as WithGlobals).nextTick()
-          
-          const to = tabs.map(el => `${document.activeElement.isSameNode(el)}`)
-          
-          return { from, to }
-        })
-
-  assert.equal(value.from, ['true', undefined, undefined])
-  assert.equal(value.to, [undefined, 'true', undefined])
-})
-
-suite(`when selectsOnFocus is false, click selects a tab`, async ({ puppeteer: { page, mouseClick } }) => {
-  await page.goto('http://localhost:3000/useTablist/withOptions')
-  await page.waitForSelector('div')
-
-  const from = await page.evaluate(() => (window as unknown as WithGlobals).testState.tablist.selected)
-  await mouseClick('div div:nth-child(2)')
-  const to = await page.evaluate(async () => (window as unknown as WithGlobals).testState.tablist.selected)
-
-  assert.is(from, 0)
-  assert.is(to, 1)
-})
-
-suite(`when selectsOnFocus is false, spacebar selects the focused tab`, async ({ puppeteer: { page } }) => {
-  await page.goto('http://localhost:3000/useTablist/withOptions')
-  await page.waitForSelector('div')
-
-  await page.evaluate(async () => document.querySelector('input').focus())
-  await page.keyboard.press('Tab')
-  await page.keyboard.press('Tab')
-
-  const from = await page.evaluate(() => (window as unknown as WithGlobals).testState.tablist.selected)
-  await page.keyboard.press(' ')
-  const to = await page.evaluate(() => (window as unknown as WithGlobals).testState.tablist.selected)
-
-  assert.is(from, 0)
-  assert.is(to, 1)
-})
-
-suite(`when selectsOnFocus is false, enter key selects the focused tab`, async ({ puppeteer: { page } }) => {
-  await page.goto('http://localhost:3000/useTablist/withOptions')
-  await page.waitForSelector('div')
-
-  await page.evaluate(async () => document.querySelector('input').focus())
-  await page.keyboard.press('Tab')
-  await page.keyboard.press('Tab')
-
-  const from = await page.evaluate(() => (window as unknown as WithGlobals).testState.tablist.selected)
-  await page.keyboard.press('Enter')
-  const to = await page.evaluate(() => (window as unknown as WithGlobals).testState.tablist.selected)
-
-  assert.is(from, 0)
-  assert.is(to, 1)
 })
 
 suite.run()
