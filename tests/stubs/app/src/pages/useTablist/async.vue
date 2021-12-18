49ba40c95345c7f0f5c7fa6bea4b2885800482cc
AlexVipond <alex.r.vipond@gmail.com>
2021-12-18 00:38:37 -0600

    testing async

---
+++ tests/stubs/app/src/pages/useTablist/async.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :ref="tablist.root.ref">
+    <div
+      v-for="({ name }, index) in organizations"
+      :key="index"
+      :ref="tablist.tabs.getRef(index)"
+    >
+      {{ name }}
+    </div>
+    <div
+      v-for="({ name, why }, index) in organizations"
+      :key="name"
+      :ref="tablist.panels.getRef(index)"
+    >
+      <p v-for="p in why">{{ p }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useFetchable } from '@baleada/vue-composition'
+import type { Organization } from '@alexvipond/mulago-foundation-portfolio'
+import { useTablist } from '../../../../../../src/interfaces'
+import { useTablistStorage } from '../../../../../../src/extensions'
+import { WithGlobals } from '../../../../../fixtures/types'
+import { tabMetadata } from '../useTablist/tabMetadata'
+
+const mulagoFoundationPortfolio = useFetchable('https://raw.githubusercontent.com/AlexVipond/mulago-foundation-portfolio/main/src/portfolio.json')
+const organizations = ref<Organization[]>([])
+
+const totalTabs = 3
+
+onMounted(async () => {
+  await mulagoFoundationPortfolio.value.get()
+  const json = await mulagoFoundationPortfolio.value.json
+
+  organizations.value = new Array(totalTabs).fill(0).map(() => json.value[Math.floor(Math.random() * json.value.length)])
+  tablist.focus.exact(0)
+})
+
+const tablist = useTablist(),
+      storage = useTablistStorage(tablist);
+
+const cleanup = () => {
+  storage.storeable.value.remove()
+  storage.storeable.value.removeStatus()
+};
+
+(window as unknown as WithGlobals).testState =  { tablist, storage, cleanup }
+</script>
