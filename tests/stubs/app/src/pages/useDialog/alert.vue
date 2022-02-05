d5c032a2d105825ea7fac4ed56a5e709eec52e73
AlexVipond <alex.r.vipond@gmail.com>
2022-02-05 10:57:20 -0600

    dialog testing

--- tests/stubs/app/src/pages/useDialog/alert.vue
+++ tests/stubs/app/src/pages/useDialog/alert.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="flex flex-col gap-10">
-    <button @click="() => dialog.open()">previously focused</button>
+    <button @click="() => dialog.open()">
+      Open dialog
+    </button>
     <div v-if="dialog.status.value === 'opened'" :ref="dialog.root.ref">
       <div class="flex flex-col gap-2">
         <span>dialog contents</span>
         <button :ref="dialog.firstFocusable.ref">first focusable</button>
-        <button @click="() => stackedDialog.open()">open stacked dialog</button>
       </div>
       <button 
         @click="() => dialog.close()"
         :ref="dialog.lastFocusable.ref"
       >last focusable</button>
-    </div>
-    <div v-if="stackedDialog.status.value === 'opened'" :ref="stackedDialog.root.ref">
-      <div class="flex flex-col gap-2">
-        <span>stackedDialog contents</span>
-        <button :ref="stackedDialog.firstFocusable.ref">first focusable</button>
-      </div>
-      <button 
-        @click="() => stackedDialog.close()"
-        :ref="stackedDialog.lastFocusable.ref"
-      >close</button>
     </div>
     <button>random focusable</button>
   </div>
@@ -30,9 +21,7 @@
 import { useDialog } from '../../../../../../src/interfaces/useDialog'
 import { WithGlobals } from '../../../../../fixtures/types';
 
-const dialog = useDialog();
-const stackedDialog = useDialog();
+const dialog = useDialog({ alerts: true });
 
 (window as unknown as WithGlobals).testState= { dialog }
-
 </script>
---
+++ tests/suites/interfaces/useDialog.test.ts
@@ -0,0 +1,111 @@
+import { suite as createSuite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { withPuppeteer } from '@baleada/prepare'
+import { WithGlobals } from '../../fixtures/types'
+
+const suite = withPuppeteer(
+  createSuite('useDialog')
+)
+
+suite(`aria roles are correctly assigned`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useDialog/withoutOptions')
+  await page.waitForSelector('div')
+
+  const value = await page.evaluate(async () => {
+    (window as unknown as WithGlobals).testState.dialog.open();
+    await (window as unknown as WithGlobals).nextTick();
+    return (window as unknown as WithGlobals).testState.dialog.root.element.value.getAttribute('role')
+  })
+
+  assert.is(value, 'dialog')
+})
+
+suite(`alertdialog role is optionally assigned`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useDialog/alert')
+  await page.waitForSelector('div')
+
+  const value = await page.evaluate(async () => {
+    (window as unknown as WithGlobals).testState.dialog.open();
+    await (window as unknown as WithGlobals).nextTick();
+    return (window as unknown as WithGlobals).testState.dialog.root.element.value.getAttribute('role')
+  })
+
+  assert.is(value, 'alertdialog')
+})
+
+suite(`open() opens dialog`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useDialog/withoutOptions')
+  await page.waitForSelector('div')
+
+  const value = await page.evaluate(async () => {
+    (window as unknown as WithGlobals).testState.dialog.open();
+    await (window as unknown as WithGlobals).nextTick();
+    return (window as unknown as WithGlobals).testState.dialog.status.value
+  })
+
+  assert.is(value, 'opened')
+})
+
+suite(`close() closes dialog`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useDialog/withoutOptions')
+  await page.waitForSelector('div')
+
+  const value = await page.evaluate(async () => {
+    (window as unknown as WithGlobals).testState.dialog.open();
+    await (window as unknown as WithGlobals).nextTick();
+    (window as unknown as WithGlobals).testState.dialog.close();
+    await (window as unknown as WithGlobals).nextTick();
+    return (window as unknown as WithGlobals).testState.dialog.status.value
+  })
+
+  assert.is(value, 'closed')
+})
+
+suite(`focuses first focusable when opened`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useDialog/withoutOptions')
+  await page.waitForSelector('div')
+
+  const value = await page.evaluate(async () => {
+    (window as unknown as WithGlobals).testState.dialog.open();
+    await (window as unknown as WithGlobals).nextTick();
+    return document.activeElement.textContent
+  })
+
+  assert.is(value, 'first focusable')
+})
+
+suite(`focuses previously focused when closed`, async ({ puppeteer: { page } }) => {
+  await page.goto('http://localhost:3000/useDialog/withoutOptions')
+  await page.waitForSelector('div')
+  await page.focus('button')
+
+  const value = await page.evaluate(async () => {
+    (window as unknown as WithGlobals).testState.dialog.open();
+    await (window as unknown as WithGlobals).nextTick();
+    (window as unknown as WithGlobals).testState.dialog.close();
+    await (window as unknown as WithGlobals).nextTick();
+    return document.activeElement.textContent
+  })
+
+  assert.is(value, 'previously focused')
+})
+
+suite(`contains focus when last focusable blurs`, async ({ puppeteer: { page, tab } }) => {
+  await page.goto('http://localhost:3000/useDialog/withoutOptions')
+  await page.waitForSelector('div')
+
+  await page.evaluate(async () => {
+    (window as unknown as WithGlobals).testState.dialog.open();
+    await (window as unknown as WithGlobals).nextTick();
+  })
+
+  // Tab past the first focusable and the button to open stacked dialog
+  // and the last focusable, back to the first focusable.
+  await tab({ direction: 'forward', total: 3 })
+
+  const value = await page.evaluate(() => document.activeElement.textContent)
+
+  assert.is(value, 'first focusable')
+})
+
+suite.run()
