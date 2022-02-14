09cc3feca491359a70cfd7de6068e29091ca4f9d
AlexVipond <alex.r.vipond@gmail.com>
2022-02-13 22:09:49 -0600

    extract event listener stuff from useFocusedAndSelected

--- src/extracted/useFocusedAndSelected.ts
+++ src/extracted/useFocusedAndSelected.ts
@@ -1,16 +1,16 @@
-import { onMounted, watch, watchPostEffect, nextTick } from 'vue'
+import { onMounted, watch, watchPostEffect } from 'vue'
 import type { Ref } from 'vue'
 import { useNavigateable, usePickable } from '@baleada/vue-composition'
-import type { Navigateable, Pickable } from '@baleada/logic'
-import { touches } from '@baleada/recognizeable-effects'
-import type { TouchesTypes, TouchesMetadata } from '@baleada/recognizeable-effects'
-import { bind, on } from '../affordances'
+import { Pickable } from '@baleada/logic'
+import type { Navigateable } from '@baleada/logic'
+import { bind } from '../affordances'
 import type { MultipleIdentifiedElementsApi } from './useElementApi'
 import { createEligibleNavigation } from './createEligibleNavigation'
 import { createEligiblePicking } from './createEligiblePicking'
 import { ensureGetStatus } from './ensureGetStatus'
 import type { StatusOption } from './ensureGetStatus'
 import { ensureWatchSourcesFromStatus } from './ensureWatchSourcesFromStatus'
+import { focusedAndSelectedOn } from '.'
 
 export type FocusedAndSelected<Multiselectable extends boolean = false> = Multiselectable extends true
   ? FocusedAndSelectedBase & {
@@ -29,6 +29,8 @@ type FocusedAndSelectedBase = {
   is: {
     focused: (index: number) => boolean,
     selected: (index: number) => boolean,
+    enabled: (index: number) => boolean,
+    disabled: (index: number) => boolean,
   }
   getStatuses: (index: number) => ['focused' | 'blurred', 'selected' | 'deselected', 'enabled' | 'disabled'],
 }
@@ -47,7 +49,9 @@ type UseFocusedAndSelectedConfigBase<Multiselectable extends boolean = false> = 
   orientation: 'horizontal' | 'vertical',
   multiselectable: Multiselectable,
   clearable: boolean,
+  popup: boolean,
   selectsOnFocus: boolean,
+  transfersFocus: boolean,
   loops: Parameters<Navigateable<HTMLElement>['next']>[0]['loops'],
   disabledElementsReceiveFocus: boolean,
   query?: Ref<string>,
@@ -61,7 +65,9 @@ export function useFocusedAndSelected<Multiselectable extends boolean = false> (
     orientation,
     multiselectable,
     clearable,
+    popup,
     selectsOnFocus,
+    transfersFocus,
     disabledElementsReceiveFocus,
     loops,
     query,
@@ -113,28 +119,32 @@ export function useFocusedAndSelected<Multiselectable extends boolean = false> (
     
     focused.value.navigate(initialFocused)
 
-    watch(
-      () => focused.value.location,
-      () => {
-        if (elementsApi.elements.value[focused.value.location]?.isSameNode(document.activeElement)) {
-          return
-        }
-        
-        elementsApi.elements.value[focused.value.location]?.focus()
-      },
-      { flush: 'post' }
-    )
-  })
-
-  bind({
-    element: elementsApi.elements,
-    values: {
-      tabindex: {
-        get: ({ index }) => index === focused.value.location ? 0 : -1,
-        watchSource: () => focused.value.location,
-      },
+    if (transfersFocus) {
+      watch(
+        () => focused.value.location,
+        () => {
+          if (elementsApi.elements.value[focused.value.location]?.isSameNode(document.activeElement)) {
+            return
+          }
+          
+          elementsApi.elements.value[focused.value.location]?.focus()
+        },
+        { flush: 'post' }
+      )
     }
   })
+
+  if (transfersFocus){
+    bind({
+      element: elementsApi.elements,
+      values: {
+        tabindex: {
+          get: ({ index }) => index === focused.value.location ? 0 : -1,
+          watchSource: () => focused.value.location,
+        },
+      }
+    })
+  }
 
 
   // SELECTED
@@ -145,6 +155,17 @@ export function useFocusedAndSelected<Multiselectable extends boolean = false> (
           elementsApi,
         }),
         deselect: FocusedAndSelected<true>['deselect'] = indexOrIndices => {
+          if (!clearable) {
+            if (
+              new Pickable(elementsApi.elements.value)
+                .pick(selected.value.picks)
+                .omit(indexOrIndices)
+                .picks.length === 0
+            ) {
+              return
+            }
+          }
+
           selected.value.omit(indexOrIndices)
         },
         isSelected: FocusedAndSelected<true>['is']['selected'] = index => selected.value.picks.includes(index)
@@ -181,374 +202,47 @@ export function useFocusedAndSelected<Multiselectable extends boolean = false> (
     ]
   }
 
-  on<
-    '!shift+!cmd+!ctrl+right'
-    | '!cmd+!ctrl+right'
-    | '!shift+!cmd+!ctrl+left'
-    | '!cmd+!ctrl+left'
-    | '!shift+ctrl+left'
-    | 'ctrl+left'
-    | '!shift+cmd+left'
-    | 'cmd+left'
-    | '!shift+ctrl+right'
-    | 'ctrl+right'
-    | '!shift+cmd+right'
-    | 'cmd+right'
-    | '!shift+!cmd+!ctrl+down'
-    | '!cmd+!ctrl+down'
-    | '!shift+!cmd+!ctrl+up'
-    | '!cmd+!ctrl+up'
-    | '!shift+ctrl+up'
-    | 'ctrl+up'
-    | '!shift+cmd+up'
-    | 'cmd+up'
-    | '!shift+ctrl+down'
-    | 'ctrl+down'
-    | '!shift+cmd+down'
-    | 'cmd+down'
-    | '+home'
-    | '+end'
-    | '+space'
-    | '+enter'
-    | 'mousedown'
-    | TouchesTypes
-    | '+esc',
-    TouchesMetadata
-  >({
-    element: elementsApi.elements,
-    effects: defineEffect => [
-      defineEffect(
-        orientation === 'horizontal'
-          ? multiselectable ? '!shift+!cmd+!ctrl+right' : '!cmd+!ctrl+right'
-          : multiselectable ? '!shift+!cmd+!ctrl+down' : '!cmd+!ctrl+down',
-        {
-          createEffect: ({ index }) => event => {
-            event.preventDefault()
-
-            const a = focus.next(index)
-
-            if (selectsOnFocus) {
-              selectOnFocus(a)
-            }
-          }
-        }
-      ),
-      defineEffect(
-        orientation === 'horizontal'
-          ? multiselectable ? '!shift+!cmd+!ctrl+left' : '!cmd+!ctrl+left'
-          : multiselectable ? '!shift+!cmd+!ctrl+up' : '!cmd+!ctrl+up',
-        {
-          createEffect: ({ index }) => event => {
-            event.preventDefault()
-            
-            const a = focus.previous(index)
-
-            if (selectsOnFocus) {
-              selectOnFocus(a)
-            }
-          }
-        }
-      ),
-      ...([
-        'home' as '+home',
-        orientation === 'horizontal'
-          ? multiselectable ? '!shift+ctrl+left' : 'ctrl+left'
-          : multiselectable ? '!shift+ctrl+up' : 'ctrl+up',
-        orientation === 'horizontal'
-          ? multiselectable ? '!shift+cmd+left' : 'cmd+left'
-          : multiselectable ? '!shift+cmd+up' : 'cmd+up',
-      ] as '!shift+cmd+left'[]).map(name => defineEffect(
-        name,
-        event => {
-          event.preventDefault()
-          
-          const a = focus.first()
-
-          if (selectsOnFocus) {
-            selectOnFocus(a)
-          }
-        }
-      )),
-      ...([
-        'end' as '+end',
-        orientation === 'horizontal'
-          ? multiselectable ? '!shift+ctrl+right' : 'ctrl+right'
-          : multiselectable ? '!shift+ctrl+down' : 'ctrl+down',
-        orientation === 'horizontal'
-          ? multiselectable ? '!shift+cmd+right' : 'cmd+right'
-          : multiselectable ? '!shift+cmd+down' : 'cmd+down',
-      ] as '!shift+cmd+right'[]).map(name => defineEffect(
-        name,
-        event => {
-          event.preventDefault()
-          
-          const a = focus.last()
-
-          if (selectsOnFocus) {
-            selectOnFocus(a)
-          }
-        }
-      )),
-      ...(() => {
-        if (selectsOnFocus) return []
-
-        return [
-          defineEffect(
-            'enter' as '+enter',
-            {
-              createEffect: ({ index }) => event => {
-                event.preventDefault()
-
-                if (isSelected(index)) {
-                  if (clearable || selected.value.picks.length > 1) {
-                    deselect(index)
-                  }
-                  
-                  return
-                }
-                
-                select.exact(index, { replace: multiselectable ? 'none' : 'all' })
-              }
-            }
-          ),
-          defineEffect(
-            'space' as '+space',
-            {
-              createEffect: ({ index }) => event => {
-                event.preventDefault()
-
-                if (query?.value) return
-
-                if (isSelected(index)) {
-                  if (clearable || selected.value.picks.length > 1) {
-                    deselect(index)
-                  }
-                  
-                  return
-                }
-                
-                select.exact(index, { replace: multiselectable ? 'none' : 'all' })
-              }
-            }
-          ),
-          defineEffect(
-            'mousedown',
-            {
-              createEffect: ({ index }) => event => {
-                event.preventDefault()
-
-                focus.exact(index)
-                
-                if (isSelected(index)) {
-                  if (clearable || selected.value.picks.length > 1) {
-                    deselect(index)
-                  }
-                  
-                  return
-                }
-      
-                select.exact(index, { replace: multiselectable ? 'none' : 'all' })
-              },
-            }
-          ),
-          defineEffect(
-            'recognizeable' as TouchesTypes,
-            {
-              createEffect: ({ index }) => event => {
-                event.preventDefault()
-
-                focus.exact(index)
-                
-                if (isSelected(index)) {
-                  if (clearable || selected.value.picks.length > 1) {
-                    deselect(index)
-                  }
-                  
-                  return
-                }
-      
-                select.exact(index, { replace: multiselectable ? 'none' : 'all' })
-              },
-              options: {
-                listenable: {
-                  recognizeable: {
-                    effects: touches()
-                  }
-                },
-              }
-            }
-          ),
-        ]
-      })(),
-      ...(() =>
-        clearable
-          ? [defineEffect(
-              'esc' as '+esc',
-              event => {
-                // Avoid unintended side effects, e.g. clearing selected and immediately
-                // closing a dialog.
-                event.preventDefault()
-                selected.value.omit()
-              }
-            )]
-          : []
-      )()
-    ],
-  })
-
-  if (multiselectable) {
-    on<
-      'shift+!cmd+!ctrl+right'
-      | 'shift+!cmd+!ctrl+left'
-      | 'shift+ctrl+left'
-      | 'shift+cmd+left'
-      | 'shift+ctrl+right'
-      | 'shift+cmd+right'
-      | 'shift+!cmd+!ctrl+down'
-      | 'shift+!cmd+!ctrl+up'
-      | 'shift+ctrl+up'
-      | 'shift+cmd+up'
-      | 'shift+ctrl+down'
-      | 'shift+cmd+down'
-      | 'cmd+a'
-      | 'ctrl+a'
-    >({
-      element: elementsApi.elements,
-      effects: defineEffect => [
-        defineEffect(
-          orientation === 'horizontal' ? 'shift+!cmd+!ctrl+right' : 'shift+!cmd+!ctrl+down',
-          {
-            createEffect: ({ index }) => event => {
-              event.preventDefault()
-
-              if (selected.value.multiple && index === selected.value.first) {
-                selected.value.omit(index)
-                focus.next(index)
-                return
-              }
-              
-              if (selected.value.multiple && index !== selected.value.last) {
-                selected.value.omit()
-              }
-
-              select.exact(index)
-              const a = select.next(index)
-
-              if (a === 'enabled') {
-                focused.value.navigate(selected.value.newest)
-              }
-            }
-          }
-        ),
-        defineEffect(
-          orientation === 'horizontal' ? 'shift+!cmd+!ctrl+left' : 'shift+!cmd+!ctrl+up',
-          {
-            createEffect: ({ index }) => event => {
-              event.preventDefault()
-
-              if (selected.value.multiple && index === selected.value.last) {
-                selected.value.omit(index)
-                focus.previous(index)
-                return
-              }
-              
-              if (selected.value.multiple && index !== selected.value.first) {
-                selected.value.omit()
-              }
-
-              select.exact(index)
-              const a = select.previous(index)
-
-              if (a === 'enabled') {
-                focused.value.navigate(selected.value.newest)
-              }
-            }
-          }
-        ),
-        defineEffect(
-          orientation === 'horizontal' ? 'shift+cmd+right' : 'shift+cmd+down',
-          {
-            createEffect: ({ index }) => event => {
-              event.preventDefault()
-
-              const picks: number[] = []
-              for (let i = index; i < elementsApi.elements.value.length; i++) {
-                if (getAbility(i) === 'enabled') {
-                  picks.push(i)
-                }
-              }
-
-              if (picks.length > 0) {
-                focus.exact(picks[picks.length - 1])
-                selected.value.pick(picks)
-              } 
-            }
-          }
-        ),
-        defineEffect(
-          orientation === 'horizontal' ? 'shift+cmd+left' : 'shift+cmd+up',
-          {
-            createEffect: ({ index }) => event => {
-              event.preventDefault()
-
-              const picks: number[] = []
-              for (let i = 0; i < index + 1; i++) {
-                if (getAbility(i) === 'enabled') {
-                  picks.push(i)
-                }
-              }
-
-              if (picks.length > 0) {
-                focus.exact(picks[0])
-                selected.value.pick(picks)
-              } 
-            }
-          }
-        ),
-        ...(['ctrl+a', 'cmd+a'] as 'cmd+a'[]).map(name => defineEffect(
-          name,
-          event => {
-            event.preventDefault()
-
-            const picks: number[] = []
-            for (let i = 0; i < elementsApi.elements.value.length; i++) {
-              if (getAbility(i) === 'enabled') {
-                picks.push(i)
-              }
-            }
-
-            if (picks.length > 0) {
-              focus.last()
-              selected.value.pick(picks, { replace: 'all' })
-            }
-          }
-        )),
-      ]
+  if (transfersFocus) {
+    focusedAndSelectedOn({
+      keyboardElement: elementsApi.elements,
+      pointerElement: elementsApi.elements,
+      getKeyboardIndex: createEffectIndex => createEffectIndex,
+      focus,
+      focused,
+      select: {
+        ...select,
+        exact: multiselectable ? select.exact : index => select.exact(index, { replace: 'all' })
+      },
+      selected,
+      deselect: multiselectable ? deselect : () => deselect(),
+      isSelected,
+      orientation,
+      multiselectable,
+      selectsOnFocus,
+      clearable,
+      popup,
+      query,
+      getAbility,
     })
   }
+  
 
-  const selectOnFocus = (a: 'enabled' | 'disabled' | 'none') => {
-    switch (a) {
-      case 'enabled':
-        selected.value.pick(focused.value.location, { replace: 'all' })
-        break
-      case 'disabled':
-        selected.value.omit()
-        break
-      case 'none':
-        // do nothing
-    }
-  }
-
+  // API
   return {
     focused,
     focus,
     selected,
-    select,
+    select: {
+      ...select,
+      exact: multiselectable ? select.exact : index => select.exact(index, { replace: 'all' })
+    },
+    deselect: multiselectable ? deselect : () => deselect(),
     is: {
       focused: index => isFocused(index),
       selected: index => isSelected(index),
+      enabled: index => getAbility(index) === 'enabled',
+      disabled: index => getAbility(index) === 'disabled',
     },
     getStatuses,
-  } as unknown as FocusedAndSelected<Multiselectable>
+  } as FocusedAndSelected<Multiselectable>
 }
