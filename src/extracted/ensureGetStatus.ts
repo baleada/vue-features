901ac40435b6f2da393dbefddcc596d82fbb5072
AlexVipond <alex.r.vipond@gmail.com>
2022-03-11 19:07:29 -0600

    support planes

--- src/extracted/ensureGetStatus.ts
+++ src/extracted/ensureGetStatus.ts
@@ -1,35 +1,51 @@
 import type { Ref } from 'vue'
+import { Plane, toAffordanceElementKind } from '.'
 import type { BindReactiveValueGetter } from '../affordances'
 import type { BindValueGetter } from './scheduleBind'
 
-export type StatusOption<Status extends string> = BindValueGetter<Status> | BindReactiveValueGetter<Status>
+export type StatusOption<B extends Ref<HTMLElement> | Ref<HTMLElement[]> | Ref<Plane<HTMLElement>>, Status extends string> = BindValueGetter<B, Status> | BindReactiveValueGetter<B, Status>
 
-export type GetStatus<Status extends string, AffordanceElementType extends Ref<HTMLElement> | Ref<HTMLElement[]>> = 
-  AffordanceElementType extends Ref<HTMLElement[]>
-    ? (index: number) => Status
-    : () => Status
+export type GetStatus<
+  B extends Ref<HTMLElement> | Ref<HTMLElement[]> | Ref<Plane<HTMLElement>>,
+  Status extends string
+> = 
+  B extends Ref<Plane<HTMLElement>>
+    ? (row: number, column: number) => Status
+    : B extends Ref<HTMLElement[]>
+      ? (index: number) => Status
+      : () => Status
 
-export function ensureGetStatus<Status extends string, AffordanceElementType extends Ref<HTMLElement> | Ref<HTMLElement[]>> (
-  { element, status }: {
-    element: AffordanceElementType,
-    status: StatusOption<Status>,
-  }
-): GetStatus<Status, AffordanceElementType> {
-  if (Array.isArray(element.value)) {
-    return (index => {
+export function ensureGetStatus<B extends Ref<HTMLElement> | Ref<HTMLElement[]> | Ref<Plane<HTMLElement>>, Status extends string> (
+  elementOrListOrPlane: B,
+  status: StatusOption<B, Status>,
+): GetStatus<B, Status> {
+  const affordanceElementKind = toAffordanceElementKind(elementOrListOrPlane)
+
+  if (affordanceElementKind === 'plane') {
+    return ((row, column) => {
       if (typeof status === 'function') {
-        return status(index)
+        return status(row, column)
       }
   
-      return status.get(index)
-    }) as GetStatus<Status, AffordanceElementType>
+      return status.get(row, column)
+    }) as GetStatus<B, Status>
   }
 
-  return () => {
+  if (affordanceElementKind === 'list') {
+    return (index => {
+      if (typeof status === 'function') {
+        return (status as BindValueGetter<Ref<HTMLElement[]>, Status>)(index)
+      }
+  
+      return (status as BindReactiveValueGetter<Ref<HTMLElement[]>, Status>).get(index)
+    }) as GetStatus<B, Status>
+  }
+
+  return (() => {
     if (typeof status === 'function') {
-      return status(0)
+      return (status as BindValueGetter<Ref<HTMLElement>, Status>)(0)
     }
 
-    return status.get(0)
-  }
+    return (status as BindReactiveValueGetter<Ref<HTMLElement>, Status>).get(0)
+  }) as GetStatus<B, Status>
 }
