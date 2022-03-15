80595eea5d9896b36ee6ffe67530788ab2946e6d
AlexVipond <alex.r.vipond@gmail.com>
2022-03-14 19:33:21 -0500

    rework plane api status for practical use cases

--- tests/stubs/app/src/pages/useElementApi/planeIdentified.vue
+++ tests/stubs/app/src/pages/useElementApi/planeIdentified.vue
@@ -2,16 +2,18 @@
   <div class="flex items-center gap-4">
     <button class="p-2 border border-gray-400 rounded" @click="() => shouldAddRow = true">add row</button>
     <button class="p-2 border border-gray-400 rounded" @click="() => shouldAddColumn = true">add column</button>
-    <button class="p-2 border border-gray-400 rounded" @click="() => shouldReorder = true">reorder</button>
+    <button class="p-2 border border-gray-400 rounded" @click="() => shouldReorderRows = true">reorder rows</button>
+    <button class="p-2 border border-gray-400 rounded" @click="() => shouldReorderFirstRow = true">reorder first row</button>
+    <button class="p-2 border border-gray-400 rounded" @click="() => shouldReorderColumns = true">reorder columns</button>
   </div>
   <div class="w-24 grid gap-4">
     <div
-      v-for="(rowNum, row) in shouldAddRow ? new Array(3).fill(0).map((_, index) => index) : new Array(2).fill(0).map((_, index) => index)"
+      v-for="(rowNum, row) in rows"
       class="grid gap-4"
       :class="[shouldAddColumn ? 'grid-cols-4' : 'grid-cols-3']"
     >
       <span
-        v-for="(colNum, column) in shouldReorder ? new Array(shouldAddColumn ? 4 : 3).fill(0).map((_, index) => index).sort((a, b) => b - a) : new Array(shouldAddColumn ? 4 : 3).fill(0).map((_, index) => index)"
+        v-for="(colNum, column) in getColumns(row)"
         :key="(rowNum * (shouldAddColumn ? 4 : 3)) + colNum"
         :ref="elementsApi.getRef(row, column)"
         class="h-full w-full flex items-center justify-center bg-emerald-600 text-emerald-50"
@@ -21,13 +23,42 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useElementApi } from '../../../../../../src/extracted/useElementApi'
 import { WithGlobals } from '../../../../../fixtures/types';
 
 const shouldAddRow = ref(false)
 const shouldAddColumn = ref(false)
-const shouldReorder = ref(false)
+const shouldReorderRows = ref(false)
+const shouldReorderColumns = ref(false)
+const shouldReorderFirstRow = ref(false)
+
+const rows = computed(() => {
+  const total = shouldAddRow.value ? 3 : 2
+
+  return shouldReorderRows.value
+    ? new Array(total).fill(0).map((_, index) => index).sort((a, b) => b - a)
+    : new Array(total).fill(0).map((_, index) => index)
+})
+
+function getColumns (r) {
+  const total = shouldAddColumn.value ? 4 : 3
+
+  let c
+
+  if (shouldReorderColumns.value) {
+    c = new Array(total).fill(0).map((_, index) => index).sort((a, b) => b - a)
+  } else {
+    c = new Array(total).fill(0).map((_, index) => index)
+  }
+
+  if (shouldReorderFirstRow.value && r === 0) {
+    c = c.slice().reverse()
+  }
+
+  return c
+}
+
 
 const elementsApi = useElementApi({ kind: 'plane', identified: true });
 
