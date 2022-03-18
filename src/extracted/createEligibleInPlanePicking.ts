c291da3175c76d6b375207fb96906c4890b24fad
AlexVipond <alex.r.vipond@gmail.com>
2022-03-18 18:55:12 -0500

    sketching

---
+++ src/extracted/createEligibleInPlanePicking.ts
@@ -0,0 +1,230 @@
+import { isRef, watch } from 'vue'
+import type { Ref } from 'vue'
+import { findIndex } from 'lazy-collections'
+import { createFilter, createReduce, Pickable } from '@baleada/logic'
+import type { IdentifiedPlaneApi } from './useElementApi'
+import { ensureWatchSources } from './ensureWatchSources'
+import { ensureGetStatus } from './ensureGetStatus'
+import type { StatusOption } from './ensureGetStatus'
+import { createToNextEligible, createToPreviousEligible } from './createToEligibleInPlane'
+import type { ToEligibility } from './createToEligibleInPlane'
+
+type BaseEligiblePickingOptions = { toEligibility?: ToEligibility }
+
+const defaultEligiblePickingOptions: BaseEligiblePickingOptions = {
+  toEligibility: () => 'eligible',
+}
+
+/**
+ * Creates methods for picking only the elements in a plane that are considered eligible,
+ * e.g. the enabled elements.
+ * 
+ * Methods return the ability of the element(s), if any, that they were able to pick.
+ */
+export function createEligibleInPlanePicking (
+  { rows, columns, ability, elementsApi }: {
+    rows: Ref<Pickable<HTMLElement[]>>,
+    columns: Ref<Pickable<HTMLElement>>,
+    ability: StatusOption<IdentifiedPlaneApi<HTMLElement>['elements'], 'enabled' | 'disabled'>,
+    elementsApi: IdentifiedPlaneApi<HTMLElement>,
+  }
+): {
+  exact: (rowOrRows: number | number[], columnOrColumns: number | number[], options?: BaseEligiblePickingOptions & Parameters<Pickable<HTMLElement>['pick']>[1]) => 'enabled' | 'none',
+  nextInRow: (row: number, column: number, options?: BaseEligiblePickingOptions & Parameters<Pickable<HTMLElement>['pick']>[1]) => 'enabled' | 'none',
+  nextInColumn: (row: number, column: number, options?: BaseEligiblePickingOptions & Parameters<Pickable<HTMLElement>['pick']>[1]) => 'enabled' | 'none',
+  previousInRow: (row: number, column: number, options?: BaseEligiblePickingOptions & Parameters<Pickable<HTMLElement>['pick']>[1]) => 'enabled' | 'none',
+  previousInColumn: (row: number, column: number, options?: BaseEligiblePickingOptions & Parameters<Pickable<HTMLElement>['pick']>[1]) => 'enabled' | 'none',
+} {
+  const getAbility = ensureGetStatus(elementsApi.elements, ability),
+        exact: ReturnType<typeof createEligibleInPlanePicking>['exact'] = (rowOrRows, columnOrColumns, options = {}) => {
+          const { toEligibility, ...pickOptions } = { ...defaultEligiblePickingOptions, ...options }
+
+          if (
+            (typeof ability === 'string' && ability === 'disabled')
+            || (isRef(ability) && ability.value === 'disabled')
+          ) {
+            return 'none'
+          }
+
+          if (
+            (typeof ability === 'string' && ability === 'enabled')
+            || (isRef(ability) && ability.value === 'enabled')
+          ) {
+            const r = new Pickable(rows.value.array).pick(rowOrRows),
+                  c = new Pickable(columns.value.array).pick(columnOrColumns)
+              
+            const eligibleRows = createFilter<number>((row, index) => toEligibility(row, c.picks[index]) === 'eligible')(r.picks),
+                  eligibleColumns = createFilter<number>((column, index) => toEligibility(r.picks[index], column) === 'eligible')(c.picks)
+            
+            rows.value.pick(eligibleRows, pickOptions)
+            columns.value.pick(eligibleColumns, pickOptions)
+            return 'enabled'
+          }
+
+          const r = new Pickable(rows.value.array).pick(rowOrRows),
+                c = new Pickable(columns.value.array).pick(columnOrColumns)
+            
+          const eligibleRows = createFilter<number>((row, index) =>
+                  getAbility(row, c.picks[index]) === 'enabled'
+                  && toEligibility(row, c.picks[index]) === 'eligible'
+                )(r.picks),
+                eligibleColumns = createFilter<number>((column, index) =>
+                  getAbility(r.picks[index], column) === 'enabled'
+                  && toEligibility(r.picks[index], column) === 'eligible'
+                )(c.picks)
+
+          if (eligibleRows.length > 0) {
+            rows.value.pick(eligibleRows, pickOptions)
+            columns.value.pick(eligibleColumns, pickOptions)
+            return 'enabled'
+          }
+
+          return 'none'
+        },
+        next: ReturnType<typeof createEligibleInPlanePicking>['next'] = (index, options = {}) => {
+          if (index === pickable.value.array.length - 1) {
+            return 'none'
+          }
+
+          const { toEligibility, ...pickOptions } = { ...defaultEligiblePickingOptions, ...options }
+
+          if (
+            (typeof ability === 'string' && ability === 'enabled')
+            || (isRef(ability) && ability.value === 'enabled')
+          ) {
+            const nextEligible = toNextEligible(index, toEligibility)
+            
+            if (typeof nextEligible === 'number') {
+              pickable.value.pick(nextEligible, pickOptions)
+              return 'enabled'
+            }
+  
+            return 'none'
+          }
+
+          const nextEligible = toNextEligible(
+            index,
+            index => getAbility(index) === 'enabled'
+              ? toEligibility(index)
+              : 'ineligible',
+          )
+            
+          if (typeof nextEligible === 'number') {
+            pickable.value.pick(nextEligible, pickOptions)
+            return 'enabled'
+          }
+
+          return 'none'
+        },
+        toNextEligible = createToNextEligible({ elementsApi, loops: false }),
+        previous: ReturnType<typeof createEligibleInPlanePicking>['next'] = (index, options = {}) => {          
+          if (index === 0) {
+            return 'none'
+          }
+
+          const { toEligibility, ...pickOptions } = { ...defaultEligiblePickingOptions, ...options }
+
+          if (
+            (typeof ability === 'string' && ability === 'enabled')
+            || (isRef(ability) && ability.value === 'enabled')
+          ) {
+            const previousEligible = toPreviousEligible(index, toEligibility)
+            
+            if (typeof previousEligible === 'number') {
+              pickable.value.pick(previousEligible, pickOptions)
+              return 'enabled'
+            }
+  
+            return 'none'
+          }
+
+          const previousEligible = toPreviousEligible(
+            index,
+            index => getAbility(index) === 'enabled'
+              ? toEligibility(index)
+              : 'ineligible',
+          )
+        
+          if (typeof previousEligible === 'number') {
+            pickable.value.pick(previousEligible, pickOptions)
+            return 'enabled'
+          }
+
+          return 'none'
+        },
+        toPreviousEligible = createToPreviousEligible({ elementsApi, loops: false })
+
+  if (isRef(ability)) {
+    watch(
+      ability,
+      () => {
+        if (ability.value === 'disabled') {
+          pickable.value.omit()
+        }
+      }
+    )
+  } else if (typeof ability !== 'string' && typeof ability !== 'function') {
+    watch(
+      ensureWatchSources(ability.watchSource),
+      () => {
+        const p = new Pickable(pickable.value.array).pick(pickable.value.picks)
+
+        p.array.forEach((_, index) => {
+          if (ability.get(index) === 'disabled') {
+            p.omit(index)
+          }
+        })
+
+        pickable.value.pick(p.picks, { replace: 'all' })
+      }
+    )
+  }
+
+  watch(
+    [elementsApi.status, elementsApi.elements],
+    (currentSources, previousSources) => {
+      const { 0: status, 1: currentElements } = currentSources,
+            { 1: previousElements } = previousSources
+
+      if (status.order === 'changed') {
+        const indices = createReduce<number, number[]>((indices, pick) => {
+          const index = findIndex<HTMLElement>(element => element.isSameNode(previousElements[pick]))(currentElements) as number
+        
+          if (typeof index === 'number') {
+            indices.push(index)
+          }
+
+          return indices
+        }, [])(pickable.value.picks)
+
+        exact(indices, { replace: 'all' })
+
+        return
+      }
+
+      if (status.length === 'shortened') {
+        const indices = createReduce<number, number[]>((indices, pick) => {
+          if (pick <= currentElements.length - 1) {
+            indices.push(pick)
+          }
+
+          return indices
+        }, [])(pickable.value.picks)
+
+        if (indices.length === 0) {
+          pickable.value.omit()
+          return
+        }
+
+        exact(indices, { replace: 'all' })
+      }
+    },
+    { flush: 'post' }
+  )
+
+  return {
+    exact,
+    next,
+    previous,
+  }
+}
