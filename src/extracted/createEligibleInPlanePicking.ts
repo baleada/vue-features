ab6c28a08f06020db99e49a9018dd48253edcd8f
AlexVipond <alex.r.vipond@gmail.com>
2022-03-19 16:28:02 -0500

    first draft of plane picking

--- src/extracted/createEligibleInPlanePicking.ts
+++ src/extracted/createEligibleInPlanePicking.ts
@@ -81,9 +81,20 @@ export function createEligibleInPlanePicking (
 
           return 'none'
         },
-        next: ReturnType<typeof createEligibleInPlanePicking>['next'] = (index, options = {}) => {
-          if (index === pickable.value.array.length - 1) {
-            return 'none'
+        nextInRow: ReturnType<typeof createEligibleInPlanePicking>['nextInRow'] = (row, column, options = { toEligibility: () => 'eligible' }) => {
+          return next('column', row, column, options)
+        },
+        nextInColumn: ReturnType<typeof createEligibleInPlanePicking>['nextInRow'] = (row, column, options = { toEligibility: () => 'eligible' }) => {
+          return next('row', row, column, options)
+        },
+        next = (iterateOver: 'row' | 'column', row: number, column: number, options: BaseEligiblePickingOptions & Parameters<Pickable<HTMLElement>['pick']>[1] = {}) => {
+          switch(iterateOver) {
+            case 'row':
+              if (row === rows.value.array.length - 1) return 'none'
+              break
+            case 'column':
+              if (column === columns.value.array.length - 1) return 'none'
+              break
           }
 
           const { toEligibility, ...pickOptions } = { ...defaultEligiblePickingOptions, ...options }
@@ -92,34 +103,57 @@ export function createEligibleInPlanePicking (
             (typeof ability === 'string' && ability === 'enabled')
             || (isRef(ability) && ability.value === 'enabled')
           ) {
-            const nextEligible = toNextEligible(index, toEligibility)
+            const nextEligible = iterateOver === 'row'
+              ? toNextEligibleInColumn(row, column, options.toEligibility)
+              : toNextEligibleInRow(row, column, options.toEligibility)
             
-            if (typeof nextEligible === 'number') {
-              pickable.value.pick(nextEligible, pickOptions)
+            if (Array.isArray(nextEligible)) {
+              rows.value.pick(nextEligible[0], pickOptions)
+              columns.value.pick(nextEligible[1], pickOptions)
               return 'enabled'
             }
   
             return 'none'
           }
 
-          const nextEligible = toNextEligible(
-            index,
-            index => getAbility(index) === 'enabled'
-              ? toEligibility(index)
-              : 'ineligible',
-          )
-            
-          if (typeof nextEligible === 'number') {
-            pickable.value.pick(nextEligible, pickOptions)
+          const nextEligible = iterateOver === 'row'
+            ? toNextEligibleInColumn(
+              row, column,
+              (r, c) => getAbility(r, c) === 'enabled'
+                ? options.toEligibility(r, c)
+                : 'ineligible',
+            )
+            : toNextEligibleInRow(
+              row, column,
+              (r, c) => getAbility(r, c) === 'enabled'
+                ? options.toEligibility(r, c)
+                : 'ineligible',
+            )
+
+          if (Array.isArray(nextEligible)) {
+            rows.value.pick(nextEligible[0], pickOptions)
+            columns.value.pick(nextEligible[1], pickOptions)
             return 'enabled'
           }
 
           return 'none'
         },
-        toNextEligible = createToNextEligible({ elementsApi, loops: false }),
-        previous: ReturnType<typeof createEligibleInPlanePicking>['next'] = (index, options = {}) => {          
-          if (index === 0) {
-            return 'none'
+        toNextEligibleInRow = createToNextEligible({ elementsApi, loops: false, iterateOver: 'column' }),
+        toNextEligibleInColumn = createToNextEligible({ elementsApi, loops: false, iterateOver: 'row' }),
+        previousInRow: ReturnType<typeof createEligibleInPlanePicking>['previousInRow'] = (row, column, options = { toEligibility: () => 'eligible' }) => {
+          return previous('column', row, column, options)
+        },
+        previousInColumn: ReturnType<typeof createEligibleInPlanePicking>['previousInRow'] = (row, column, options = { toEligibility: () => 'eligible' }) => {
+          return previous('row', row, column, options)
+        },
+        previous = (iterateOver: 'row' | 'column', row: number, column: number, options: BaseEligiblePickingOptions & Parameters<Pickable<HTMLElement>['pick']>[1] = {}) => {
+          switch(iterateOver) {
+            case 'row':
+              if (row === 0) return 'none'
+              break
+            case 'column':
+              if (column === 0) return 'none'
+              break
           }
 
           const { toEligibility, ...pickOptions } = { ...defaultEligiblePickingOptions, ...options }
@@ -128,38 +162,51 @@ export function createEligibleInPlanePicking (
             (typeof ability === 'string' && ability === 'enabled')
             || (isRef(ability) && ability.value === 'enabled')
           ) {
-            const previousEligible = toPreviousEligible(index, toEligibility)
+            const previousEligible = iterateOver === 'row'
+              ? toPreviousEligibleInColumn(row, column, options.toEligibility)
+              : toPreviousEligibleInRow(row, column, options.toEligibility)
             
-            if (typeof previousEligible === 'number') {
-              pickable.value.pick(previousEligible, pickOptions)
+            if (Array.isArray(previousEligible)) {
+              rows.value.pick(previousEligible[0], pickOptions)
+              columns.value.pick(previousEligible[1], pickOptions)
               return 'enabled'
             }
   
             return 'none'
           }
 
-          const previousEligible = toPreviousEligible(
-            index,
-            index => getAbility(index) === 'enabled'
-              ? toEligibility(index)
-              : 'ineligible',
-          )
+          const previousEligible = iterateOver === 'row'
+            ? toPreviousEligibleInColumn(
+              row, column,
+              (r, c) => getAbility(r, c) === 'enabled'
+                ? options.toEligibility(r, c)
+                : 'ineligible',
+            )
+            : toPreviousEligibleInRow(
+              row, column,
+              (r, c) => getAbility(r, c) === 'enabled'
+                ? options.toEligibility(r, c)
+                : 'ineligible',
+            )
         
-          if (typeof previousEligible === 'number') {
-            pickable.value.pick(previousEligible, pickOptions)
+          if (Array.isArray(previousEligible)) {
+            rows.value.pick(previousEligible[0], pickOptions)
+            columns.value.pick(previousEligible[1], pickOptions)
             return 'enabled'
           }
 
           return 'none'
         },
-        toPreviousEligible = createToPreviousEligible({ elementsApi, loops: false })
+        toPreviousEligibleInRow = createToPreviousEligible({ elementsApi, loops: false, iterateOver: 'column' }),
+        toPreviousEligibleInColumn = createToPreviousEligible({ elementsApi, loops: false, iterateOver: 'row' })
 
   if (isRef(ability)) {
     watch(
       ability,
       () => {
         if (ability.value === 'disabled') {
-          pickable.value.omit()
+          rows.value.omit()
+          columns.value.omit()
         }
       }
     )
@@ -167,15 +214,42 @@ export function createEligibleInPlanePicking (
     watch(
       ensureWatchSources(ability.watchSource),
       () => {
-        const p = new Pickable(pickable.value.array).pick(pickable.value.picks)
+        const positions: [row: number, column: number][] = []
 
-        p.array.forEach((_, index) => {
-          if (ability.get(index) === 'disabled') {
-            p.omit(index)
+        for (let row = 0; row < rows.value.array.length; row++) {
+          for (let column = 0; column < columns.value.array.length; column++) {
+            positions.push([row, column])
           }
-        })
+        }
 
-        pickable.value.pick(p.picks, { replace: 'all' })
+        const p = new Pickable(positions)
+
+        for (let rowPick = 0; rowPick < rows.value.picks.length; rowPick++) {
+          const indexInPositions = findIndex(
+            ([row, column]) => row === rows.value.picks[rowPick] && column === columns.value.picks[rowPick]
+          )(positions) as number
+          
+          p.pick(indexInPositions)
+        }
+
+        for (const pick of p.picks) {
+          const [row, column] = p.array[pick]
+          if (ability.get(row, column) === 'disabled') {
+            p.omit(pick)
+          }
+        }
+
+        const newRows: number[] = [],
+              newColumns: number[] = []
+
+        for (const pick of p.picks) {
+          const [row, column] = p.array[pick]
+          newRows.push(row)
+          newColumns.push(column)
+        }
+
+        rows.value.pick(newRows, { replace: 'all' })
+        columns.value.pick(newColumns, { replace: 'all' })
       }
     )
   }
@@ -187,36 +261,76 @@ export function createEligibleInPlanePicking (
             { 1: previousElements } = previousSources
 
       if (status.order === 'changed') {
-        const indices = createReduce<number, number[]>((indices, pick) => {
-          const index = findIndex<HTMLElement>(element => element.isSameNode(previousElements[pick]))(currentElements) as number
-        
-          if (typeof index === 'number') {
-            indices.push(index)
+        const withPositions: [position: [row: number, column: number], element: HTMLElement][] = []
+
+        for (let row = 0; row < rows.value.array.length; row++) {
+          for (let column = 0; column < columns.value.array.length; column++) {
+            withPositions.push([[row, column], currentElements[row][column]])
           }
+        }
 
-          return indices
-        }, [])(pickable.value.picks)
+        const newRows: number[] = [],
+              newColumns: number[] = []
 
-        exact(indices, { replace: 'all' })
+        for (let rowPick = 0; rowPick < rows.value.picks.length; rowPick++) {
+          const indexInWithPositions = findIndex<typeof withPositions[0]>(
+            ([_, element]) => element.isSameNode(previousElements[rows.value.picks[rowPick]][columns.value.picks[rowPick]])
+          )(withPositions) as number
+
+          if (typeof indexInWithPositions === 'number') {
+            const [row, column] = withPositions[indexInWithPositions][0]
+            newRows.push(row)
+            newColumns.push(column)
+          }
+        }
+
+        exact(newRows, newColumns, { replace: 'all' })
 
         return
       }
 
-      if (status.length === 'shortened') {
-        const indices = createReduce<number, number[]>((indices, pick) => {
-          if (pick <= currentElements.length - 1) {
-            indices.push(pick)
+      if (status.rowLength === 'shortened' || status.columnLength === 'shortened') {
+        const newRows: number[] = [],
+              newColumns: number[] = []
+
+        if (status.rowLength === 'shortened' && status.columnLength === 'shortened') {
+          for (let rowPick = 0; rowPick < rows.value.picks.length; rowPick++) {
+            if (
+              rows.value.picks[rowPick] < rows.value.array.length
+              && columns.value.picks[rowPick] < columns.value.array.length
+            ) {
+              newRows.push(rows.value.picks[rowPick])
+              newColumns.push(columns.value.picks[rowPick])
+            }
           }
 
-          return indices
-        }, [])(pickable.value.picks)
-
-        if (indices.length === 0) {
-          pickable.value.omit()
+          exact(newRows, newColumns, { replace: 'all' })
           return
         }
-
-        exact(indices, { replace: 'all' })
+        
+        if (status.rowLength === 'shortened') {
+          for (let rowPick = 0; rowPick < rows.value.picks.length; rowPick++) {
+            if (rows.value.picks[rowPick] < currentElements.length) {
+              newRows.push(rows.value.picks[rowPick])
+              newColumns.push(columns.value.picks[rowPick])
+            }
+          }
+          
+          exact(newRows, newColumns, { replace: 'all' })
+          return
+        }
+        
+        if (status.columnLength === 'shortened') {
+          for (let rowPick = 0; rowPick < rows.value.picks.length; rowPick++) {
+            if (columns.value.picks[rowPick] < currentElements[rows.value.picks[rowPick]].length) {
+              newRows.push(rows.value.picks[rowPick])
+              newColumns.push(columns.value.picks[rowPick])
+            }
+          }
+          
+          exact(newRows, newColumns, { replace: 'all' })
+          return
+        }
       }
     },
     { flush: 'post' }
@@ -224,7 +338,9 @@ export function createEligibleInPlanePicking (
 
   return {
     exact,
-    next,
-    previous,
+    nextInRow,
+    nextInColumn,
+    previousInRow,
+    previousInColumn,
   }
 }
