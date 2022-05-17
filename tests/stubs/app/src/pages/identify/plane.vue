0c54ad30ae2dc02228e4176e0501117c02018d1e
AlexVipond <alex.r.vipond@gmail.com>
2022-05-16 22:43:52 -0500

    update tests for planes

---
+++ tests/stubs/app/src/pages/identify/plane.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid grid-rows-2 grid-cols-3">
+    <template v-for="(row, rowIndex) in rows">
+      <span
+        v-for="(column, columnIndex) in columns"
+        :key="`${row},${column}`"
+        :ref="api.getRef(rowIndex, columnIndex)"
+      >{{ `${row},${column}` }}</span>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { identify } from '../../../../../../src/affordances/identify'
+import { useElementApi } from '../../../../../../src/extracted/useElementApi'
+import type { WithGlobals } from '../../../../../fixtures/types'
+
+const api = useElementApi({ kind: 'plane' }),
+      rows = ref([0, 1]),
+      columns = ref([0, 1, 2]),
+      ids = identify(api.elements)
+
+;(window as unknown as WithGlobals).testState =  { ids }
+</script>
