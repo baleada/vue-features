2cdd6e610f72236b6ddf96fbe8a7ec9a025bb63d
AlexVipond <alex.r.vipond@gmail.com>
2022-06-28 00:08:32 -0500

    add aria-owns test

---
+++ tests/stubs/app/src/pages/useListbox/withOptions.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Input is just a focus target for testing tab navigation -->
+  <input type="text" />
+  <div :ref="listbox.root.ref">
+    <div
+      v-for="(option, index) in optionMetadata"
+      :key="index"
+      :ref="listbox.options.getRef(index)"
+      style="display: flex; gap: 0.5rem; align-items: center; padding: 0.5rem;"
+    >
+      <span>{{ option }}</span>
+      <span v-show="listbox.is.focused(index)">⭐</span>
+      <span v-show="listbox.is.selected(index)">✅</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { useListbox, UseListboxOptions } from '../../../../../../src/interfaces'
+import { WithGlobals } from '../../../../../fixtures/types';
+import { optionMetadata } from './optionMetadata'
+
+const listbox = reactive(useListbox({
+  needsAriaOwns: true,
+}))
+
+;(window as unknown as WithGlobals).testState =  { listbox }
+</script>
