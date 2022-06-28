282203bb720ee5f3bc5ed007f7ec8390cc4cfbf3
AlexVipond <alex.r.vipond@gmail.com>
2022-06-27 23:49:47 -0500

    updating tests

--- tests/stubs/app/src/pages/createEligibleInPlanePicking/abilityReactiveGetter.vue
+++ tests/stubs/app/src/pages/createEligibleInPlanePicking/abilityReactiveGetter.vue
@@ -4,7 +4,7 @@
       <div v-for="(r, row) in itemsRef" class="grid grid-cols-10 gap-4">
         <div
           v-for="(c, column) in itemsRef"
-          :ref="elementsApi.getRef(row, column)"
+          :ref="plane.getRef(row, column)"
           class="h-[3em] w-[3em] flex items-center justify-center p-1 bg-blue-200 text-blue-900 font-mono rounded"
         >
           {{ `${r}.${c}` }}
@@ -16,23 +16,23 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { useNavigateable } from '@baleada/vue-composition';
+import { usePickable } from '@baleada/vue-composition';
 import { createReorder } from '@baleada/logic';
 import { useElementApi } from '../../../../../../src/extracted';
-import { createEligibleInPlaneNavigation } from '../../../../../../src/extracted/createEligibleInPlaneNavigation';
+import { createEligibleInPlanePicking } from '../../../../../../src/extracted/createEligibleInPlanePicking';
 import { WithGlobals } from '../../../../../fixtures/types';
 import { items } from './items'
 
 const itemsRef = ref(items);
 
-const elementsApi = useElementApi({ kind: 'plane', identified: true });
+const plane = useElementApi({ kind: 'plane', identified: true });
 
-const rows = useNavigateable<HTMLElement[]>([])
-const columns = useNavigateable<HTMLElement>([])
+const rows = usePickable<HTMLElement[]>([])
+const columns = usePickable<HTMLElement>([])
 
 onMounted(() => {
-  rows.value.array = elementsApi.elements.value
-  columns.value.array = elementsApi.elements.value[0]
+  rows.value.array = plane.elements.value
+  columns.value.array = plane.elements.value[0]
 })
 
 const abilities = ref(new Array(10).fill(new Array(10).fill('disabled')))
@@ -42,19 +42,17 @@ const ability = (row, column) => abilities.value[row][column]
 ;(window as unknown as WithGlobals).testState = {
   rows,
   columns,
-  elementsApi,
+  plane,
   ability,
   abilities,
-  eligibleNavigation: createEligibleInPlaneNavigation({
-    disabledElementsAreEligibleLocations: false,
+  eligiblePicking: createEligibleInPlanePicking({
     rows,
     columns,
-    loops: false,
     ability: {
       get: ability,
       watchSource: abilities,
     },
-    elementsApi,
+    plane,
   }),
   reorder: () => itemsRef.value = createReorder<number>(0, 9)(itemsRef.value),
   remove: () => itemsRef.value = itemsRef.value.slice(0, 5),
