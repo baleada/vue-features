9fa5ec5451892a9bbf1181ea8ff1ce100453f98b
AlexVipond <alex.r.vipond@gmail.com>
2022-10-27 11:09:40 -0500

    fancier useSize type safety

--- src/extensions/useSize.ts
+++ src/extensions/useSize.ts
@@ -1,21 +1,30 @@
 import { ref, computed } from 'vue'
 import type { Ref } from 'vue'
+import { createMap, createReduce } from '@baleada/logic'
 import { on } from '../affordances'
 import type { OnEffectConfig } from '../affordances'
 import { toEntries, ensureElementFromExtendable } from '../extracted'
 import type { Extendable } from '../extracted'
 
-export type Size = {
+export type Size<Breakpoints extends Record<string, number>> = {
   rect: Ref<DOMRectReadOnly>,
-  breaks: Ref<{ [breakpoint: string] : boolean }>,
+  breaks: Ref<Record<keyof Breakpoints, boolean>>,
   orientation: Ref<'none' | 'portrait' | 'landscape'>,
 }
 
-export type UseSizeOptions = {
-  breakpoints?: { [breakpoint: string]: number },
+export type UseSizeOptions<Breakpoints extends Record<string, number>> = {
+  breakpoints?: Breakpoints,
 } & OnEffectConfig<HTMLElement, 'resize'>['options']['listen']
 
-const defaultOptions: UseSizeOptions = {
+const tailwindBreakpoints = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  '2xl': 1536,
+} as const
+
+const defaultOptions: UseSizeOptions<typeof tailwindBreakpoints> = {
   // Defaults to Tailwind breakpoints
   breakpoints: {
     sm: 640,
@@ -26,10 +35,10 @@ const defaultOptions: UseSizeOptions = {
   }
 }
 
-export function useSize (
+export function useSize<Breakpoints extends Record<string, number>> (
   extendable: Extendable,
-  options: UseSizeOptions = {}
-): Size {
+  options: UseSizeOptions<Breakpoints> = {}
+): Size<Breakpoints> {
   // OPTIONS
   const { breakpoints } = { ...defaultOptions, ...options }
 
@@ -52,14 +61,19 @@ export function useSize (
 
 
   // BREAKS
-  const sorted = toEntries(breakpoints).sort(([, pixelsA], [, pixelsZ]) => pixelsZ - pixelsA),
-        withZero: [string, number][] = sorted[0][1] > 0 ? [['zero', 0], ...sorted] : sorted,
-        assertions = withZero.map(([, p]) => pixels => pixels >= p),
-        breaks: Size['breaks'] = computed(() => assertions.reduce((breaks, assertion, index) => {
-          const breakpoint = withZero[index][0]
-          breaks[breakpoint] = rect.value ? assertion(rect.value.width) : false
-          return breaks
-        }, {}))
+  const sorted = toEntries(breakpoints).sort(([, pixelsA], [, pixelsZ]) => pixelsZ - pixelsA) as [keyof Breakpoints, number][],
+        withZero: [keyof Breakpoints | 'zero', number][] = sorted[0][1] > 0 ? [['zero', 0], ...sorted] : sorted,
+        assertions = createMap<typeof withZero[0], (pixels: number) => boolean>(
+          ([, p]) => pixels => pixels >= p
+        )(withZero),
+        breaks: Size<Breakpoints>['breaks'] = computed(() => createReduce<typeof assertions[0], Size<Breakpoints>['breaks']['value']>(
+          (breaks, assertion, index) => {
+            const breakpoint = withZero[index][0]
+            breaks[breakpoint] = rect.value ? assertion(rect.value.width) : false
+            return breaks
+          },
+          {} as Size<Breakpoints>['breaks']['value']
+        )(assertions))
 
   
   // ORIENTATION
