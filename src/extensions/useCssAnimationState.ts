42894397a530056f3360ef556d8192a52ab99cf9
AlexVipond <alex.r.vipond@gmail.com>
2022-12-13 17:56:19 -0600

    sketch transition and animation state

---
+++ src/extensions/useCssAnimationState.ts
@@ -0,0 +1,47 @@
+import { ref, computed } from 'vue'
+import type { ComputedRef } from 'vue'
+import { on } from '../affordances'
+import { ensureElementFromExtendable } from '../extracted'
+import type { Extendable } from '../extracted'
+
+export type CssAnimationState = {
+  status: ComputedRef<'ready' | 'animating' | 'animated'>,
+  elapsedTime: ComputedRef<number>,
+  iterations: ComputedRef<number>,
+}
+
+export function useCssAnimationState (extendable: Extendable) {
+  const element = ensureElementFromExtendable(extendable),
+        status = ref<CssAnimationState['status']['value']>('ready'),
+        elapsedTime = ref<CssAnimationState['elapsedTime']['value']>(),
+        iterations = ref<CssAnimationState['iterations']['value']>(0)
+
+  on(
+    element,
+    {
+      animationstart: event => {
+        iterations.value = 0
+        status.value = 'animating'
+        elapsedTime.value = event.elapsedTime
+      },
+      animationiteration: event => {
+        iterations.value += 1
+        elapsedTime.value = event.elapsedTime
+      },
+      animationend: event => {      
+        status.value = 'animated'
+        elapsedTime.value = event.elapsedTime
+      },
+      animationcancel: event => {
+        status.value = 'ready'
+        elapsedTime.value = event.elapsedTime
+      },
+    },
+  )
+
+  return {
+    status: computed(() => status.value),
+    elapsedTime: computed(() => elapsedTime.value),
+    iterations: computed(() => iterations.value),
+  }
+}
---
+++ src/extensions/useCssTransitionState.ts
@@ -0,0 +1,39 @@
+import { ref, computed } from 'vue'
+import type { ComputedRef } from 'vue'
+import { on } from '../affordances'
+import { ensureElementFromExtendable } from '../extracted'
+import type { Extendable } from '../extracted'
+
+export type CssTransitionState = {
+  status: ComputedRef<'ready' | 'transitioning' | 'transitioned'>,
+  elapsedTime: ComputedRef<number>,
+}
+
+export function useCssTransitionState (extendable: Extendable) {
+  const element = ensureElementFromExtendable(extendable),
+        status = ref<CssTransitionState['status']['value']>('ready'),
+        elapsedTime = ref<CssTransitionState['elapsedTime']['value']>()
+
+  on(
+    element,
+    {
+      transitionstart: event => {
+        status.value = 'transitioning'
+        elapsedTime.value = event.elapsedTime
+      },
+      transitionend: event => {      
+        status.value = 'transitioned'
+        elapsedTime.value = event.elapsedTime
+      },
+      transitioncancel: event => {
+        status.value = 'ready'
+        elapsedTime.value = event.elapsedTime
+      },
+    },
+  )
+
+  return {
+    status: computed(() => status.value),
+    elapsedTime: computed(() => elapsedTime.value),
+  }
+}
