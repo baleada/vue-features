5e63857c53d7b9fad81a6a3490a5e6e34ca24872
Alex Vipond <hello@alexvipond.dev>
2023-12-31 00:27:33 -0600

    tweak and test useConditionalRendering

---
+++ tests/stubs/app/src/pages/useConditionalRendering/withOptions.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span v-if="rendering.is.rendered()" ref="el"></span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useConditionalRendering } from '../../../../../../src/extensions/useConditionalRendering'
+
+const el = ref<HTMLSpanElement>(null)
+const rendering = useConditionalRendering(el, {
+  initialRenders: false,
+})
+
+window.testState = { rendering, el }
+</script>
