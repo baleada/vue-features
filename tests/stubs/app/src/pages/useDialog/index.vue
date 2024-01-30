00199d26547e053e371ba494320c8cd89e1613be
Alex Vipond <hello@alexvipond.dev>
2024-01-30 10:18:25 -0600

    tests

---
+++ tests/stubs/app/src/pages/useDialog/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :ref="dialog.root.ref()"></div>
+</template>
+
+<script setup lang="tsx">
+import { useDialog } from '../../../../../../src/interfaces/useDialog'
+import { getOptions } from '../../getOptions'
+
+const dialog = useDialog(getOptions())
+
+window.testState = { dialog }
+</script>
