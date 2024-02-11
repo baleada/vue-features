5feb42a9def029b264f68594bde027de7c5b9087
Alex Vipond <hello@alexvipond.dev>
2024-02-10 19:11:12 -0600

    test useSelect

--- tests/suites/combos/useSelect.test.ts
+++ tests/suites/combos/useSelect.test.ts
@@ -6,8 +6,65 @@ const suite = withPlaywright(
   createSuite('useSelect')
 )
 
-suite.skip('is tested', async ({ playwright: { page } }) => {
-  assert.ok(false)
+suite('open() works', async ({ playwright: { page } }) => {
+  await page.goto('http://localhost:5173/useSelect/withUrlOptions')
+  await page.waitForSelector('div', { state: 'attached' })
+
+  const value = await page.evaluate(async () => {
+    window.testState.select.listbox.open()
+    await window.nextTick()
+    return window.testState.select.listbox.popupStatus.value
+  })
+
+  assert.is(value, 'opened')
+})
+
+suite('close() works', async ({ playwright: { page } }) => {
+  await page.goto('http://localhost:5173/useSelect/withUrlOptions')
+  await page.waitForSelector('div', { state: 'attached' })
+
+  const value = await page.evaluate(async () => {
+    window.testState.select.listbox.open()
+    await window.nextTick()
+    window.testState.select.listbox.close()
+    await window.nextTick()
+    return window.testState.select.listbox.popupStatus.value
+  })
+
+  assert.is(value, 'closed')
+})
+
+suite('button interactions open listbox', async ({ playwright: { page } }) => {
+  await page.goto('http://localhost:5173/useSelect/withUrlOptions')
+  await page.waitForSelector('div', { state: 'attached' })
+
+  
+  {
+    await page.evaluate(() => window.testState.select.button.root.element.value.focus())
+    await page.keyboard.press('Enter')
+
+    const value = await page.evaluate(() => window.testState.select.listbox.popupStatus.value),
+          expected = 'opened'
+
+    assert.is(value, expected)
+  }
+
+  await page.evaluate(() => window.testState.select.listbox.close())
+})
+
+suite('focuses focused listbox option when opened', async ({ playwright: { page } }) => {
+  await page.goto('http://localhost:5173/useSelect/withUrlOptions')
+  await page.waitForSelector('div', { state: 'attached' })
+
+  const value = await page.evaluate(async () => {
+          window.testState.select.listbox.open()
+          await window.nextTick()
+          await window.nextTick()
+          return window.testState.select.listbox.options.list.value.findIndex(el => el === document.activeElement)
+        }),
+        expected = 0
+
+  assert.is(value, expected)
 })
 
 suite.run()
