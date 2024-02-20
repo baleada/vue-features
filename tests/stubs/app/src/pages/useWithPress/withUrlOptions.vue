bb98e2d6646a21df86ddb04577c9762b24a0fb91
Alex Vipond <hello@alexvipond.dev>
2024-02-19 23:27:07 -0600

    test useWithPress

---
+++ tests/stubs/app/src/pages/useWithPress/withUrlOptions.vue
@@ -0,0 +1,13 @@
+<template>
+  <button ref="el">with press</button>
+</template>
+
+<script setup lang="tsx">
+import { ref } from 'vue'
+import { useWithPress } from '../../../../../../src/extensions/useWithPress'
+import { getOptions } from '../../getOptions';
+
+const el = ref()
+
+window.testState = { withPress: useWithPress(el, getOptions()) }
+</script>
